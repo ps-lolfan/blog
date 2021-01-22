d2f7297992ba068ed40554fd391ebe7730b3695e
Praveen Sinnur <praveen@zeliot.in>
2021-01-22 12:26:24 +0530

    tile set for page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import HomePage from "./Pages/HomePage";
+import { Helmet } from "react-helmet";
+import DirectionsBikeIcon from "@material-ui/icons/DirectionsBike";
+
 function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>ps-lolfan zone</title>
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://ibb.co/Ctw1yPY"
+          sizes="16x16"
+        />
+      </Helmet>
       <HomePage />
     </div>
   );
