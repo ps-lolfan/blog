e5056e05a161edfb6ecf06a75a4f340e154baa49
Praveen Sinnur <praveen@zeliot.in>
2021-01-22 13:27:05 +0530

    checking to merge

--- src/Pages/HomePage/HomeGround/HomeGround.jsx
+++ src/Pages/HomePage/HomeGround/HomeGround.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Profile from "../Profile";
 
 export default class HomeGround extends Component {
   render() {
     return (
-      <div>
-        <h1>This is home ground</h1>
-      </div>
+      <Grid container spacing={3} justify="space-around">
+        <Grid item xs={4}>
+          <Profile />
+        </Grid>
+        <Grid item xs={4}>
+          <Profile />
+        </Grid>
+      </Grid>
     );
   }
 }
